858f62397476760252ceb4e3a6c4d5e5ede8343a
grow-a-garden0 <ranieltmolinas@gmail.com>
2025-07-13 10:00:32 +0800

    Update No-Lag.lua

--- No-Lag.lua
+++ No-Lag.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/grow-a-garden0/Grow-grow/refs/heads/main/2710da8e01628b4362dd7e5a836d6b58.txt"))()
+loadstring(game:HttpGet("https://pastefy.app/IOq2COsm/raw"))()
